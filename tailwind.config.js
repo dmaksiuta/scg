483febf5ec12078b732e575c839ada71a717664b
dmaksiuta <shutle369@gmail.com>
2024-02-13 23:09:51 +0200

    added basic structure for desktop and tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,45 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      white: '#fff',
+      primary: "#FF643C",
+      active: "#1432B4",
+      link: "#999",
+      gray: "#e5e5e5",
+      footer_link: "#cecece",
+    },
     extend: {
       fontFamily: {
-        'sans': ['"Roboto"', ...defaultTheme.fontFamily.sans],
+        sans: ['"Roboto"', ...defaultTheme.fontFamily.sans],
       },
-    }
+      size: {
+        38: "2.375rem",
+        60: "3.75rem",
+      },
+      height: {
+        60: "3.75rem",
+        28: "1.75rem",
+      },
+      width: {
+        164: "10.25rem",
+      },
+      padding: {
+        30: "1.875rem",
+      },
+      translate: {
+        'center': '-50%'
+      }
+    },
+    screens: {
+      xl: "1145px",
+    },
+    container: {
+      center: true,
+      padding: "16rem",
+    },
   },
   plugins: [],
 };
