4e212e1a0a3643fa8a5889f15cdfb077737310a5
dmaksiuta <shutle369@gmail.com>
2024-02-13 18:33:16 +0200

    added roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Roboto"', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [],
-}
+};
